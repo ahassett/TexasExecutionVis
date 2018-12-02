c7cf35d1ab4626d866dedd60f95d0dc355055acd
Lambus <tianzhil@middlebury.edu>
2018-12-01 22:57:32 -0500

    Random death dots work

--- geo/js_geo_dead_dots.js
+++ geo/js_geo_dead_dots.js
@@ -1,3 +1,4 @@
+const SVG = d3.select(".texas")
 
 d3.csv("http://127.0.0.1:80/_data/Execution.csv").then(function(data){
 	console.log(data);
@@ -6,52 +7,58 @@ d3.csv("http://127.0.0.1:80/_data/Execution.csv").then(function(data){
 		.key((d) => d["County"])
 		.map(data);
 
+	let circle_g = SVG.append("g");
+
+	let color_scale = d3.scaleOrdinal(d3.schemeCategory10)
+		.domain(["Hispanic", "Black", "White"]);
+
 	function getBoundingBoxCenter (selection) {
 		// get the DOM element from a D3 selection
 		// you could also use "this" inside .each()
 		var element = selection.node();
 		// use the native SVG interface to get the bounding box
 		var bbox = element.getBBox();
-		// return the center of the bounding box
-		return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+		// randomly generate a point within path
+		let passed = false, newX = 0, newY = 0;
+
+		while(!passed) {
+			var pointInSvgPolygon = require("point-in-svg-polygon");
+			/*
+			// See the path specification for the correct format
+			// https://developer.mozilla.org/en/docs/Web/SVG/Tutorial/Paths
+			var pathString = selection.attr("d");
+			// Parse the string
+
+			for (i = 0; i < pathString.length; i++){
+				if (pathString[i] != "M" && pathString[i].toLowerCase() != pathString[i].toUpperCase()){
+					pathString = pathString.slice(0, i) + " " + pathString.slice(i);
+					i++;
+				}
+			}*/
+			newX = bbox.x + Math.floor(Math.random() * bbox.width);
+			newY = bbox.y + Math.floor(Math.random() * bbox.height);
+
+			// passed = pointInSvgPolygon.isInside([newX, newY], pathString);
+			passed = true;
+		}
+
+		return [newX, newY];
+
 	}
 
-	var BB = getBoundingBoxCenter(d3.select("#Andrews"));
+	data.forEach(function(d){
+		that = d3.select("#" + d["County"].replace(" ", "_")) ;
 
-	d3.select("svg").append("circle")
-		.attr("cx", BB[0])
-		.attr("cy", BB[1])
-		.attr("r", 50);
+		circle_g.append("circle")
+			.attr("cx", getBoundingBoxCenter(that)[0])
+			.attr("cy", getBoundingBoxCenter(that)[1])
+			.attr("r", 50)
+			.attr("fill", color_scale(d["Race"]));
 
-
-	/*
-	// color by criminal count
-	byCounty.each(function(val, key){
-		let county_name = key;
-
-		if (d3.select("#" + key)["_groups"][0][0] == null) county_name = key.replace(" ", "_");
-
-		d3.select("#" + county_name)
-		 	.style("fill", color_scale(val.length));
-	});
-
-	// hover: add tooltip
-	d3.selectAll(".cls-2").on("mouseover", function() {
-		let coord = [d3.event.pageX, d3.event.pageY];
-
-		d3.select("#tooltip") // select tooltip
-			.classed("hidden", false)
-			.style("left", (coord[0]) + 25 + "px")
-			.style("top", (coord[1]) + 25 + "px");
-
-		d3.select("#county_name").text(d3.select(this).attr("id"));
-		d3.select("#number_executions").text(byCounty.get(d3.select(this).attr("id")).length);
-	});
-
-	// mouseout: hide tooltip
-	d3.selectAll(".cls-2").on("mouseout", function(){
-		d3.select("#tooltip").classed("hidden", true);
 	})
-	*/
+
+
+
+
 
 });
