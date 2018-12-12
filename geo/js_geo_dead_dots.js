3e4851bd99e477be30e311b54438890b8728ee7a
Lambus <tianzhil@middlebury.edu>
2018-12-12 00:05:33 -0500

    Map comments done

--- geo/js_geo_dead_dots.js
+++ geo/js_geo_dead_dots.js
@@ -2,17 +2,19 @@
 function vis_map(parentDOM, width, height, data) {
 	console.log(data);
 
+	// nest data by county
 	const byCounty = d3.nest()
 		.key((d) => d["County"])
 		.map(data);
 
-	let circle_g = parentDOM.append("g");
-	let legend_g = parentDOM.append("g")
+	let circle_g = parentDOM.append("g"); // g-container for circles
+	let legend_g = parentDOM.append("g") // g-container for legend
 		.attr("transform", `translate(${10000}, ${-250})`);
 
 	let color_scale = d3.scaleOrdinal(d3.schemeCategory10)
 		.domain(["Hispanic", "Black", "White"]);
 
+	// function to draw circles given filtered data
 	function drawCircles(dataset) {
 		function getBoundingBoxCenter (selection) {
 			// get the DOM element from a D3 selection
@@ -21,12 +23,12 @@ function vis_map(parentDOM, width, height, data) {
 			// use the native SVG interface to get the bounding box
 			var bbox = element.getBBox();
 			// randomly generate a point within path
-
 			let newX = bbox.x + Math.floor(Math.random() * bbox.width);
 			let newY = bbox.y + Math.floor(Math.random() * bbox.height);
 			return [newX, newY];
 		}
 
+		// animation to remove all circles before switching a year
 		circle_g.selectAll("circle")
 			.transition()
 			.attr("r", 10)
@@ -34,11 +36,10 @@ function vis_map(parentDOM, width, height, data) {
 			.duration(500)
 			.remove();
 
-		// circle_g.html("");
-
 		dataset.forEach(function(d){
 			that = d3.select("#" + d["County"].replace(" ", "_")) ;
 
+			// append the circles at the right positions
 			circle_g.append("circle")
 				.attr("cx", getBoundingBoxCenter(that)[0])
 				.attr("cy", getBoundingBoxCenter(that)[1])
@@ -53,6 +54,7 @@ function vis_map(parentDOM, width, height, data) {
 		});
 	}
 
+	// initial circle display
 	drawCircles(data);
 
 
@@ -86,7 +88,7 @@ function vis_map(parentDOM, width, height, data) {
 		.style("font-size", "300px")
 		.text((d)=> d)
 
-	// event handler for legend
+	// event handler for legend circle as checkbox
 	legend_circles.on("click", function(d){
 
 		// if deselected
@@ -107,17 +109,20 @@ function vis_map(parentDOM, width, height, data) {
 		}
 	});
 
-	/*
+	/*------------
 	* Freaking animation
-	*/
+	------------*/
+	// g-container for play/pause button
 	const trigger = parentDOM.append("g")
 		.attr("transform", "translate(7000, -250)")
 		.classed("handpointer", true);
 
+	// g-container for stop button
 	const detrigger = parentDOM.append("g")
 		.attr("transform", "translate(8500, -250)")
 		.classed("handpointer", true);
 
+	// rectangle for play/pause button
 	trigger.append("rect")
 		.attr("x", 0)
 		.attr("y", 0)
@@ -125,6 +130,7 @@ function vis_map(parentDOM, width, height, data) {
 		.attr("height", 1000)
 		.attr("fill", "black");
 
+	// rectangle for stop button
 	detrigger.append("rect")
 		.attr("x", 0)
 		.attr("y", 0)
@@ -132,6 +138,7 @@ function vis_map(parentDOM, width, height, data) {
 		.attr("height", 1000)
 		.attr("fill", "orange");
 
+	// the text that alters between play and pause
 	let indicator = trigger.append('text')
 		.style("font-size", "350px")
 		.attr("fill", "white")
@@ -139,6 +146,7 @@ function vis_map(parentDOM, width, height, data) {
 		.attr("y", 600)
 		.text("Play")
 
+	// "stop" text
 	detrigger.append("text")
 		.style("font-size", "350px")
 		.attr("fill", "white")
@@ -146,7 +154,8 @@ function vis_map(parentDOM, width, height, data) {
 		.attr("y", 600)
 		.text("Stop");
 
-	// slider
+	// slider parameters
+	// currVal is the current coordinate of the handle
 	let slide_width = 6000;
 	let startDate = new Date("01/01/1982");
 	let endDate = new Date("12/31/2018");
@@ -155,17 +164,20 @@ function vis_map(parentDOM, width, height, data) {
 	let targetVal = slide_width;
 	let timer;
 
+	// the scale for the slide
 	let x_slide = d3.scaleTime()
 		.domain([startDate, endDate])
 		.range([0, slide_width])
 		.clamp(true);
 
-	// components of sliders
+	// g-container for components of sliders
 	let slider = parentDOM.append("g")
 		.attr("class", "slider")
 		.attr("transform", "translate(500, -300)");
 
 	// the main slide
+	// mainly copy-pasted from
+	// https://bl.ocks.org/officeofjane/47d2b0bfeecfcb41d2212d06d095c763?fbclid=IwAR2SLniIdZRIONKoq55sPrDL38kXUsB8QoQiw26FmDvdf5L3pVF6vDk2ego
 	slider.append("line")
 		.attr("class", "track")
 		.attr("x1", x_slide.range()[0])
@@ -182,7 +194,7 @@ function vis_map(parentDOM, width, height, data) {
 			})
 		);
 
-	// the ticks
+	// the ticks of the slide
 	slider.insert("g", ".track-overlay")
 		.attr("class", "ticks")
 		.attr("transform", "translate(0, 250)")
@@ -197,10 +209,12 @@ function vis_map(parentDOM, width, height, data) {
 		.text(function(d){return formatDateIntoYear(d); })
 		.style("font-size", "200px");
 
+	// the handle of the slide
 	let handle = slider.insert("circle", ".track-overlay")
 		.attr("class", "handle")
 		.attr("r", 150);
 
+	// the label of the slide
 	let label = slider.append("text")
 		.attr("class", "label")
 		.attr("text-anchor", "middle")
@@ -208,8 +222,14 @@ function vis_map(parentDOM, width, height, data) {
 		.attr("y", -170)
 		.style("font-size", "200px")
 		.text(formatDateIntoYear(startDate))
-		// .attr("transfrom", "translate(500, 0)");
 
+	/*
+	* Update the visualization: slider and circles
+	* that = current dateTime (e.g. Jan 2005)
+	* that = previous dateTime (e.g. Dec 2004)
+	* Slider is always updated
+	* Circles are only updated when we switch from one year to another
+	*/
 	function update(that, thee) {
 		// update position and text of label according to slider scale
 		handle.attr("cx", x_slide(that));
@@ -232,6 +252,11 @@ function vis_map(parentDOM, width, height, data) {
 			drawCircles(newData);
 	}
 
+	/*
+	* Each step of the interval
+	* Each step takes 100ms
+	* The whole animation takes 200 steps
+	*/
 	function step(){
 		currVal = currVal + (targetVal / 200);
 
@@ -240,8 +265,11 @@ function vis_map(parentDOM, width, height, data) {
 		let thatYear = formatDateIntoYear(that);
 		let theeYear = formatDateIntoYear(thee);
 
+		// update visualization
 		update(that, thee);
-		// tec 1
+
+		// story 1: show "#tec1", 1983
+		// and pause
 		if (thatYear >= 1983 && theeYear < 1983){
 			d3.selectAll(".texas_text")
 				.classed("hidden", true)
@@ -253,7 +281,8 @@ function vis_map(parentDOM, width, height, data) {
 			indicator.text("Play");
 		}
 
-		// tec 2
+		// story 2: show "#tec2", 1998
+		// and pause
 		if (thatYear >= 1998 && theeYear < 1998){
 			d3.selectAll(".texas_text")
 				.classed("hidden", true)
@@ -265,7 +294,8 @@ function vis_map(parentDOM, width, height, data) {
 			indicator.text("Play");
 		}
 
-		// tec 3
+		// story 3: show "#tec3", 2005
+		// and pause
 		if (thatYear >= 2005 && theeYear < 2005){
 			d3.selectAll(".texas_text")
 				.classed("hidden", true)
@@ -277,22 +307,26 @@ function vis_map(parentDOM, width, height, data) {
 			indicator.text("Play");
 		}
 
+		// if the handle reaches the end of the slider, reset
 		if (currVal > targetVal) {
-			drawCircles(data);
+			drawCircles(data); // draw all circles
 			moving = false;
 			currVal = 0;
 			clearInterval(timer);
 			indicator.text("Play");
 
 			d3.selectAll(".texas_text")
-				.classed("hidden", true)
+				.classed("hidden", true);
+
+			// ending story: show "#tec3", 2005
 			d3.select("#tec4")
-				.classed("hidden", false)
+				.classed("hidden", false);
 		}
 	}
 
 
 	// Animation starts upon clicking play button
+	// pauses upon clicking pause button
 	trigger.on("click", function(){
 		if (indicator.text() == "Pause") {
 			moving = false;
@@ -306,6 +340,7 @@ function vis_map(parentDOM, width, height, data) {
 		}
 	});
 
+	// upon clicking stop button, everything is reset to be initial
 	detrigger.on("click", function(){
 		d3.selectAll(".texas_text").classed("hidden", true);
 		d3.select("#tec0").classed("hidden", false);
@@ -316,8 +351,7 @@ function vis_map(parentDOM, width, height, data) {
 		clearInterval(timer);
 		indicator.text("Play");
 
-
-
+		// reset label and handle to the left 
 		handle.attr("cx", x_slide(startDate));
 
 		label.attr("x", x_slide(startDate))
